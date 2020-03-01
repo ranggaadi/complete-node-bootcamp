3a9c29d1ada7f7952f3e1f65b529c0e7e284e5f9
ranggadi <adirangga922@gmail.com>
2020-03-01 14:20:50 +0700

    trying express api post to rewrite hardcoded json file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //     // res.status(200).send('Output dari server');
 //     res.status(200).json({message: "Ini API dari get method", appName: "Natours", List: [{nama: "Abdul", usia: 18}]});
@@ -26,6 +28,24 @@ app.get('/api/v1/tours', (req, res) => {
         data : {
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+    // res.send("Done!");
+
+    const newId = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({id : newId}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tours: newTour
+            }
+        })
     })
 })
 
