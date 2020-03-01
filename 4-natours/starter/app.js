4a8e20e4d86edc8d2bca8de3990d5f8fa124e4b3
ranggadi <adirangga922@gmail.com>
2020-03-01 13:23:59 +0700

    trying express api get read json file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,15 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 const port = 3000;
-app.get('/', (req, res) => {
-    // res.status(200).send('Output dari server');
-    res.status(200).json({message: "Ini API dari get method", appName: "Natours", List: [{nama: "Abdul", usia: 18}]});
-    //jika menggunakan json maka Content/Type akan otomatis dirubah menjadi json/application
-})
 
-app.post('/', (req, res)=> {
-    res.status(403).send("Tidak Bisa post pada endpoint ini...");
+// app.get('/', (req, res) => {
+//     // res.status(200).send('Output dari server');
+//     res.status(200).json({message: "Ini API dari get method", appName: "Natours", List: [{nama: "Abdul", usia: 18}]});
+//     //jika menggunakan json maka Content/Type akan otomatis dirubah menjadi json/application
+// })
+
+// app.post('/', (req, res)=> {
+//     res.status(403).send("Tidak Bisa post pada endpoint ini...");
+// })
+
+
+// Ini adalah top level code (dieksekusi sekali aja)
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: "success",
+        result: tours.length,
+        data : {
+            tours
+        }
+    })
 })
 
 app.listen(port, () => {
