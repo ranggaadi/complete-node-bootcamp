c3467a26b17cccfb7edf3a9e1675bc5f19242785
ranggadi <adirangga922@gmail.com>
2020-03-01 14:49:18 +0700

    url parameter to get single item

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -31,6 +31,26 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1; //merubah string params id menjadi number
+    const tour = tours.find(el => el.id === id);
+
+    if(!tour){
+        res.status(404).json({
+            status: "fail",
+            message: "Invalid ID"
+        })
+    }else{
+        res.status(200).json({
+            status: "success",
+            data: {
+                tour
+            }
+        })
+    }
+})
+
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
     // res.send("Done!");
