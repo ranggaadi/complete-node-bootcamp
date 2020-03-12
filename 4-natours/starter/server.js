ddacf2ee395448463a980c4156eb6b8a5773af0d
ranggadi <adirangga922@gmail.com>
2020-03-12 09:46:41 +0700

    creating simple Schema and model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({path:'./config.env'});
+
 mongoose.connect(process.env.DATABASE_LOCAL, {
     useFindAndModify: false,
     useNewUrlParser: true,
@@ -12,8 +13,27 @@ mongoose.connect(process.env.DATABASE_LOCAL, {
 .then(() => console.log("Connection to DB established"));
 
 const app = require('./app');
-
 // console.log(process.env);
+
+//dibawah ini adalah schema
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, "A tour must have a price"]
+    }
+})
+
+//dibawah ini adalah model
+const Tour = mongoose.model("Tour", tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
