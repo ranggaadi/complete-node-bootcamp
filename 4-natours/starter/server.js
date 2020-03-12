ffc0c412c3ca89497c8bbedd2d85fb2926099743
ranggadi <adirangga922@gmail.com>
2020-03-12 09:28:47 +0700

    established connection to local DB

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,19 @@
 // ### SERVER START
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({path:'./config.env'});
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connection to DB established"));
 
 const app = require('./app');
 
-console.log(process.env);
+// console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
