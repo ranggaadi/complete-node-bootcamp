60c2f34e5fdee2427a28c5c21ca29b772e2b54a0
ranggadi <adirangga922@gmail.com>
2020-03-17 14:51:53 +0700

    modularizing API feature

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIfeatures = require('./../utils/apiFeatures');
 // const fs = require('fs');
 
 // // top level code untuk meng json kan file tours-simple.json
@@ -21,68 +22,14 @@ exports.alias = (req, res, next) => {
 exports.getAllTours = async (req, res) => {
     try {
 
-        // 1a. Simple Filtering
-        // console.log(req.query);
-        const queryObj = {...req.query} //mengcopy isi dari req.query
-        const exclude = ['limit', 'sort', 'page', 'fields'];
-        exclude.forEach(el => delete queryObj[el]); //menghapus seua property yang ada di exclude
-        // console.log(queryObj);
-
-        // 1b. Advanced Filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`); //mereplace gt/gte dkk menjadi $gt / $gte dkk
-        filteredQuery = JSON.parse(queryStr);
-
-
-
-
-        let query = Tour.find(filteredQuery); //ini akan mereturn query sehingga dapat di chain
-
-
-
-        
-        // 2.SORT FUNCTION
-        console.log(req.query);
-        if(req.query.sort){
-            const sortQuery = req.query.sort.split(',').join(' '); //memisahkan dua kondisi sort yang berbeda
-            // console.log(sortQuery);
-            query = query.sort(sortQuery); //sort dengan dua kondisi pada mongoose seperti (sort 1 sort2) dipisahkan dengan spasi
-        }else{
-            query = query.sort('-createdAt') //secara default akan mengurutkan dari yang terbaru.
-            //apabila fungsi sort terdapat minus berarti secara descending
-        }
-
-
-
-
-        //3 Field Limiting / SELECT / Projection
-        // console.log(req.query);
-
-        if(req.query.fields){
-            const project = req.query.fields.split(',').join(' ');
-            // console.log(project);
-            query = query.select(project);
-        }else{
-            query = query.select('-__v'); //tanda minus pada select() berarti tidak akan ditampilkan
-        }
-
-
-        //4.Pagination skip() dan limit
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 10;
-        const skip = (page-1)*limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if(req.query.page){
-            const numOfData = await Tour.estimatedDocumentCount(); //menghitung banyaknya document
-            if(skip >= numOfData) throw new Error("This page doesn't exist");
-            //apabila nantinya page melebihi banyaknya data maka nanti akan di throw sebuah error.
-        }
-
-        
         // const tours = await Tour.find();
-        const tours =  await query; //tidak menggunakan await karena nantinya query akan dichaining sehingga harus dipisah
+        const feature = new APIfeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .project()   //sama seperti select pada db
+            .paginate();
+
+        const tours = await feature.query; //tidak menggunakan await karena nantinya query akan dichaining sehingga harus dipisah
 
         res.status(200).json({
             status: "success",
@@ -113,7 +60,7 @@ exports.getTour = async (req, res) => {
                 tour
             }
         })
-    }catch (err) {
+    } catch (err) {
         res.status(400).json({
             status: "fail",
             requestedAt: req.reqTime,
@@ -128,7 +75,7 @@ exports.createATour = async (req, res) => {
         // newTour.save()
 
         const newTour = await Tour.create(req.body);
-        
+
         res.status(201).json({
             status: "success",
             requestedAt: req.reqTime,
@@ -136,7 +83,7 @@ exports.createATour = async (req, res) => {
                 tour: newTour
             }
         });
-    } catch(err) {
+    } catch (err) {
         res.status(400).json({
             status: "fail",
             requestedAt: req.reqTime,
@@ -146,7 +93,7 @@ exports.createATour = async (req, res) => {
 }
 
 exports.updateTour = async (req, res) => {
-    try{
+    try {
         const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
             new: true, //akan mereturn yang telah diupdate
             runValidators: true //akan melalukan pengecekan lagi sesuai schema
@@ -159,7 +106,7 @@ exports.updateTour = async (req, res) => {
                 tour
             }
         })
-    }catch (err) {
+    } catch (err) {
         res.status(400).json({
             status: "fail",
             requestedAt: req.reqTime,
@@ -178,7 +125,7 @@ exports.deleteTour = async (req, res) => {
             requestedAt: req.reqTime,
             data: null
         });
-    }catch (err) {
+    } catch (err) {
         res.status(404).json({
             status: "fail",
             requestedAt: req.reqTime,
@@ -187,13 +134,13 @@ exports.deleteTour = async (req, res) => {
     }
 }
 
-exports.checkTop5Tour =  (req, res, next) => {
-    try{
-        if(req.url == '/top-5-tours'){
-            req.query = {limit: 5, sort: '-ratingsAverage,price'};
+exports.checkTop5Tour = (req, res, next) => {
+    try {
+        if (req.url == '/top-5-tours') {
+            req.query = { limit: 5, sort: '-ratingsAverage,price' };
         }
         next();
-    }catch(err){
+    } catch (err) {
         res.status(404).json({
             status: "fail",
             message: err.message
---
+++ 4-natours/starter/utils/apiFeatures.js
@@ -0,0 +1,64 @@
+class APIfeatures {
+    constructor(query, requestQuery) {
+        this.query = query;
+        this.requestQuery = requestQuery;
+    }
+
+    filter() {
+        // 1a. Simple Filtering
+        // console.log(req.query);
+        const queryObj = { ...this.requestQuery } //mengcopy isi dari req.query
+        const exclude = ['limit', 'sort', 'page', 'fields'];
+        exclude.forEach(el => delete queryObj[el]); //menghapus seua property yang ada di exclude
+        // console.log(queryObj);
+
+        // 1b. Advanced Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`); //mereplace gt/gte dkk menjadi $gt / $gte dkk
+        let filteredQuery = JSON.parse(queryStr);
+
+        this.query = this.query.find(filteredQuery); //ini akan mereturn query sehingga dapat di chain
+        return this; //mereturn object kelas ini agar bisa di chaining dengan fungsi lainnya.
+    }
+
+    sort() {
+        if (this.requestQuery.sort) {
+            const sortQuery = this.requestQuery.sort.split(',').join(' '); //memisahkan dua kondisi sort yang berbeda
+            // console.log(sortQuery);
+            this.query = this.query.sort(sortQuery); //sort dengan dua kondisi pada mongoose seperti (sort 1 sort2) dipisahkan dengan spasi
+        } else {
+            this.query = this.query.sort('-createdAt') //secara default akan mengurutkan dari yang terbaru.
+            //apabila fungsi sort terdapat minus berarti secara descending
+        }
+
+        return this; //return this agar bisa dichain dengan yang lain.
+    }
+
+    project() {
+        //3 Field Limiting / SELECT / Projection
+        // console.log(req.query);
+
+        if (this.requestQuery.fields) {
+            const project = this.requestQuery.fields.split(',').join(' ');
+            // console.log(project);
+            this.query = this.query.select(project);
+        } else {
+            this.query = this.query.select('-__v'); //tanda minus pada select() berarti tidak akan ditampilkan
+        }
+
+        return this;
+    }
+
+    paginate() {
+        //4.Pagination skip() dan limit
+        const page = this.requestQuery.page * 1 || 1;
+        const limit = this.requestQuery.limit * 1 || 10;
+        const skip = (page - 1) * limit;
+
+        this.query = this.query.skip(skip).limit(limit);
+
+        return this;
+    }
+}
+
+module.exports = APIfeatures;
