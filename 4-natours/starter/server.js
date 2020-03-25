fa52dbf7fa81daafe6a56db29697ce7bd4470a8d
ranggadi <adirangga922@gmail.com>
2020-03-25 15:31:21 +0700

    handling unhandled exception

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -16,6 +16,23 @@ const app = require('./app');
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App starting on port ${port}`);
+});
+
+
+//unhandled rejection biasa terjadi pada kesalahan DB (terjadi diluar applikasi express) maka perlu ditangani
+//untuk mengetes kasusnya rubah value port dari variabel DATABASE_LOCAL pada config.env lalu save server.js,
+//apabila masih baik baik saja coba lakukan request.
+
+process.on('unhandledRejection', (err) => { //process mendengarkan event unhandledRejection
+    console.log(err.name, err.message);
+    console.log("UNHANDLED REJECTION. Shutting down . . ."); //yang hanya bisa dilakukan programmer adalah menutup server
+    
+    //kenapa tidak langsung menggunakan process.exit() karena agar memproses request yang ada dulu, sehingga tidak langsung
+    //tertutup ketika user lain masih menunggu respon.
+
+    server.close(()=> { 
+        process.exit(1); //parameter pada exit mendefinisikan 0 = success, 1 unchaught exception.
+    })
 })
