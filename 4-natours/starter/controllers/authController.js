545b8bc722d929d187853c615c6f0cf0b56578ea
ranggadi <adirangga922@gmail.com>
2020-03-25 21:48:25 +0700

    signing up user (JWT)

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,11 +1,22 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next)=> {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    });
+
+    const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE_TIME
+    })
 
     res.status(201).json({
         status: "success",
+        token,
         requestedAt: req.reqTime,
         data: {
             user: newUser
