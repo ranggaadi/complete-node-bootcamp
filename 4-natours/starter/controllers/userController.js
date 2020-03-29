0616684b73197d2c014cd15955d0ac94ae85ef92
ranggadi <adirangga922@gmail.com>
2020-03-29 11:01:27 +0700

    updating profile current user

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,8 +1,16 @@
 const catchAsync = require('./../utils/catchAsync');
 const User = require('./../models/userModel');
+const CustomError = require('./../utils/customError');
 
+const filter = (obj, ...allowedFields) => {
+    let newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    })
+    return newObj;
+}
 
-exports.getAllUsers = catchAsync(async(req, res, next) => {
+exports.getAllUsers = catchAsync(async (req, res, next) => {
     const users = await User.find();
 
     res.status(200).json({
@@ -26,6 +34,29 @@ exports.getUser = (req, res) => {
         message: 'This API not yet implemented'
     })
 }
+exports.updateMe = catchAsync( async(req, res, next) => {
+    // 1.) Apabila fields request mengandung password / confirmPassword maka kirim error
+    if (req.body.password || req.body.confirmPassword) {
+        return next(new CustomError('This route is not for password updates. Please use /update-password instead.', 400));
+    }
+
+    // 2.) filter body agar tidak merubah data sensitive seperti role dkk
+    filteredBody = filter(req.body, 'name', 'email');
+
+    // 3.) update data user
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true, runValidators: true
+    });
+
+    res.status(200).json({
+        status: "success",
+        requestedAt: req.reqTime,
+        data: {
+            user: updatedUser
+        }
+    })
+});
+
 exports.updateUser = (req, res) => {
     res.status(500).json({
         status: 'error',
--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -8,6 +8,7 @@ router.post('/login', authController.login)
 router.post('/forgot-password', authController.forgotPassword)
 router.patch('/reset-password/:token', authController.resetPassword)
 router.patch('/update-password', authController.protect, authController.updatePassword);
+router.patch('/update-profile', authController.protect, usersController.updateMe);
 
 router.route('/')
 .get(usersController.getAllUsers)
