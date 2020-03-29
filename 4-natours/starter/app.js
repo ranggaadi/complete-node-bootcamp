31e6ed9685e10607eadc984083ee05348162ab90
ranggadi <adirangga922@gmail.com>
2020-03-29 16:23:35 +0700

    securing http headers

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,6 @@
 const morgan = require('morgan');
 const express = require('express');
+const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const app = express();
 
@@ -11,30 +12,35 @@ const userRouter = require('./routes/userRoutes');
 // #### GLOBAL MIDDLEWARE
 // NB : pada middleware harus selalu ada next
 
-//third party middleware
+// set security for HTTP Headers
+app.use(helmet());
+
+//third party middleware untuk logging di development
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
 // menggunakan express rate limit sehingga dapat menghindari serangan DOS dengan melimit request dalam satuan waktu
 // variabel limiter akan mereturn sebuah middleware
-
 const limiter = rateLimit({
     max: 3,
     windowMs: 60*60*1000, //dalam satu jam tiap ip diberi limit 100
     message: "Too many request from this IP, please try again in 1 hour!" //pesan yang akan dikirim jika melewati batas
 })
-
 app.use('/api', limiter); //limit hanya akan berlaku pada request pada API
 
-app.use(express.json());
+//melimit body dari req.body sehingga apabila lebih dari 10kb akan ditolak
+app.use(express.json({limit: "10kb"}));
 
+//serve static file
 app.use(express.static(`${__dirname}/public`)); //menjadikan folder public menjadi root
 
-app.use((req, res, next) => {
-    console.log("Halo dari middleware");
-    next(); 
-})
+//contoh penggunaan middleware
+// app.use((req, res, next) => {
+//     console.log("Halo dari middleware");
+//     next(); 
+// })
+
 
 //simple middleware untuk mencatat waktu request
 app.use((req, res, next) => {
