071d0afdab3cace79d3089a1182dbd6a0b08213b
rangga adi <adirangga922@gmail.com>
2020-05-18 13:06:22 +0700

    building tour overview page

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+
+exports.getOverview = catchAsync(async(req, res, next) => {
+    //1) get the data
+    const tours = await Tour.find();
+    //2) build the tempalate from data from step 1 by passing it in overview pages
+
     res.status(200).render('pages/overview', {
-        title: "All Tours"
+        title: "All Tours",
+        tours
     })
-}
+});
 
 exports.getTour = (req, res) => {
     res.status(200).render('pages/tour', {
