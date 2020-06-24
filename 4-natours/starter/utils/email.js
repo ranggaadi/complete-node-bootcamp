356f4ffcc9851ea1c2eb75e1f1111f1099ac83ab
rangga adi <adirangga922@gmail.com>
2020-06-24 14:37:15 +0700

    building email handler

--- 4-natours/starter/utils/email.js
+++ 4-natours/starter/utils/email.js
@@ -1,29 +1,54 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-    // ## LANGKAHNYA ADA 3 YANG UTAMA
-
-    // 1.) membuat apa yang disebut dengan transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    })
-
-    // 2.) mendefinisikan opsi dari mail sender
-    const mailOption = {
-        from: "Natours Travel <test@natour.com>",
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-        // html: ""
+module.exports = class Email {
+    constructor(user, url){
+        this.to = user.email
+        this.firstName = user.firstName
+        this.url = url
+        this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`
     }
 
-    // 3.) kirim email
-    await transporter.sendMail(mailOption);
-}
+    newTransporter() {
+        if(process.env.NODE_ENV === "production"){
+            //nanti akan menggunakan sendgrid
+            return 1
+        }
 
-module.exports = sendEmail;
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        })
+    }
+
+    async send(template, subject){
+        //mengambil renderan hasil pug ke html sebagai strings
+        const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
+
+        //membuat mail option
+        const mailOption = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html),
+        }
+
+        //membuat transporter dan mengirimnya
+        await this.newTransporter().sendMail(mailOption);
+
+    }
+
+    async sendWelcome(){
+        await this.send('welcome', 'Welcome to Natours Family!')
+    }
+}
